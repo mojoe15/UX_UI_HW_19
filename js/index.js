f7e97ba41b75f888ca9fff79a93e4e96688c0324
mojoe15 <eliranger15@gmail.com>
2021-10-22 17:35:37 -0400

    added btn styles to resume and submit form btn

--- js/index.js
+++ js/index.js
@@ -5,10 +5,11 @@ $("#submit-form-button").click(function () {
 $("#dark-mode").click(function () {
     if ($("#dark-mode").text() === "Dark Mode") {
         $("#dark-mode").text("Light Mode");
-        $(".hero").css("background-color", "black");
-        $(".hero").css("color", "white");
+        $(".hero").css("background-color", "rgb(53,48,42)");
+        $("*").css("color", "rgb(252, 251, 240)");
     } else {
         $("#dark-mode").text("Dark Mode");
         $(".hero").css("background-color", "inherit");
+        $("*").css("color", "rgb(95, 97, 90)");
     }
 });
