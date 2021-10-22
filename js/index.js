fff53c466355a37badb6506448418eff0bc369a3
mojoe15 <eliranger15@gmail.com>
2021-10-22 15:39:51 -0400

    adding dark mode function and changing color palette

--- js/index.js
+++ js/index.js
@@ -4,8 +4,9 @@ $("#submit-form-button").click(function () {
 
 $("#dark-mode").click(function () {
     if ($("#dark-mode").text() === "Dark Mode") {
-        $("#dark-mode").text("Switch back to Light Mode");
+        $("#dark-mode").text("Light Mode");
         $(".hero").css("background-color", "black");
+        $(".hero").css("color", "white");
     } else {
         $("#dark-mode").text("Dark Mode");
         $(".hero").css("background-color", "inherit");
